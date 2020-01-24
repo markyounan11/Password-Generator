55d1b0bfd852233792d40633e9f78f819209a89f
Mark Younan <markyounan@gmail.com>
2020-01-24 13:03:28 -0800

    added final changes, wrote comments, and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-Generator
+
+This application generates a random password from an array of characters and prints the password to the screen.
+
+Users are prompted to select how long they'd like their password to be. The number they enter must be between 8 and 128. If a number is chosen outside of this range, the user will be alerted that they haven't met the criteria and will then be prompted to enter a different number.
+
+After the user has entered a valid number for the length of their password, the user will then be prompted to select whether they'd like to include or exclude characters in their password from 3 different categories. The categories are numbers, lowercase letters, and uppercase letters.
+
+Based on the user's input, the characters they've selected to include in the generated password are then pushed to a master array that contains the arrays of character categories they have chosen. From there, a function randomly chooses the correct number of characters that will make up the random password.
+
+The generated password is then printed to the screen for the user to see. After the password has been printed, the user can then click the copy button on the screen which will trigger a function that will copy the generated password to the local clipboard. The user has the option to paste this copied password anywhere on their machine.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//global variables
 let passwordLength = 0;
 let passwordLengthChecker = Number(passwordLength);
 let length=0;
@@ -6,9 +7,15 @@ let masterArray = [];
 let lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numArr = [0,1,2,3,4,5,6,7,8,9];
+let useNumbers;
+let useLowerCase;
+let useUpperCase;
 
 
 
+
+
+//grabbing html elements
 let generateBtn = document.querySelector("#gen");
 let copyBtn = document.querySelector("#copy");
 let passwordBox = document.querySelector("#pass-final");
@@ -16,6 +23,8 @@ let passwordBox = document.querySelector("#pass-final");
 
 
 
+
+//prompts user to choose password length and alerts if not within range
 function chooseLengthCorrectly() {
     passwordLength = prompt("How long would you like to make your password?");
     passwordLengthChecker = Number(passwordLength);
@@ -27,10 +36,45 @@ function chooseLengthCorrectly() {
         chooseCriteria();
     }
 }
-    chooseLengthCorrectly();
 
 
 
+
+
+//selects contents of master array based on user input
+function generateMasterArray(num, upper, lower) {
+    console.log("num"+ num);
+
+    if(num==true){
+        for(let i=0; i<numArr.length;i++){
+            masterArray.push(numArr[i]);
+        }
+    }
+    console.log("upper"+ upper);
+
+    if(upper==true){
+        for(let i=0; i<lowerArr.length;i++){
+            masterArray.push(lowerArr[i]);
+        }
+    }
+    console.log("lower"+ lower);
+
+    if(lower==true){
+        for(let i=0; i<upperArr.length;i++){
+            masterArray.push(upperArr[i]);
+        }
+    }
+        
+    console.log(masterArray);
+        //call fx to call seceret word (we need to pass in the master array, the isNum, isUpper, isLower)
+    generateSecretWord(masterArray, num, upper, lower);
+}
+
+
+
+
+
+//prompts user to select whether or not to include characters from each criteria
 function chooseCriteria() {
     alert("Please choose from the 3 criteria");
     let useNumbers = confirm("Would you like to use numerical characters?");
@@ -40,58 +84,14 @@ function chooseCriteria() {
     while (useLowerCase == false && useNumbers == false && useUpperCase === false) {
         chooseCriteria();
     }
-
-
-
-}
-generateSecretWord();
-
-let generateSecretWord = function () {
-    //event.preventDefault();
-    let secretWord = "";
-    for(let i=0;i<passwordLengthChecker;i++) {
-    secretWord+=masterArray[Math.floor(Math.random()*((masterArray.length-1)-0)+0)];
-    console.log(secretWord);
-}
-    passwordBox.textContent = secretWord;
-}
-generateSecretWord();
-
-
-
-
-function generateMasterArray() {
-
-    if(useNumbers==true){
-        for(let i=0; i<numArr.length;i++){
-            masterArray.push(numArr[i]);
-        }
-    }
-    
-    if(useLowerCase==true){
-        for(let i=0; i<lowerArr.length;i++){
-            masterArray.push(lowerArr[i]);
-        }
-    }
-    
-    if(useUpperCase){
-        for(let i=0; i<upperArr.length;i++){
-            masterArray.push(upperArr[i]);
-        }
-            // masterArray.push(upperArr[ Math.floor(Math.random() * upperArr.length) ]);
-    }
-    
-    console.log(masterArray);
-    return masterArray;
+    generateMasterArray(useNumbers,useUpperCase,useLowerCase);
 }
 
 
 
 
 
-
-
-
+//allows password to be copied to clipboard
 let copyText = function () {
     let copyText = document.getElementById("pass-final");
     let textArea = document.createElement("textarea");
@@ -105,132 +105,28 @@ let copyText = function () {
 
 
 
-generateBtn.addEventListener("click", generateSecretWord);
+
+
+//to do: need to create a checking mech to make sure all test cases met
+//how are we going to do that?
+let generateSecretWord = function (masterArray, isNum, isUpper, isLower) {
+    console.log(masterArray);
+    //event.preventDefault();
+    let secretWord = "";
+    for(let i=0;i<passwordLengthChecker;i++) {
+        //we will have conditionals occur, once done, let it do the normal randomization
+        //we know secretword is a string.. typeof() fx that returns out type
+        //there is a includes function that takes in a string and returns true or false
+    secretWord+=masterArray[Math.floor(Math.random()*((masterArray.length-1)-0)+0)];
+    console.log(secretWord);
+}
+    passwordBox.textContent = secretWord;
+}
+
+
+
+
+
+//on click listeners
+generateBtn.addEventListener("click", chooseLengthCorrectly);
 copyBtn.addEventListener("click" , copyText);
-
-
-
-
-
-
-
-
-
-
-//dynamically append secretword to frontend
-//get element by ID
-//event listener for button
-//create function that captures secretWord
-//call secretWord
-
-
-
-//set 
-//then call ran values from the array based on the length
-
-//return the value
-//append to frontend
-
-// shuffle method
-
-
-
-
-
-// alert("Welcome to Lawrence's Password Generator!");
-// let passLength = prompt("How many characters would you like your password to be? Must be between 8 and 128.");
-// let passLengthChecker = Number(passLength);
-// let charTypeSpec = confirm("Would you like these special characters, which includes a space?  !\"/#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-// let charTypeNum = confirm("Would you like numeric characters 0-9?");
-// let charTypeLower = confirm("Would you like lowercase characters?");
-// let charTypeUpper = confirm("Would you like uppercase characters?");
-
-// // Varibles for buttons
-
-// let generateBtn = document.querySelector("#gen");
-// let copyBtn = document.querySelector("#copy");
-// let passwordBox = document.querySelector("#pass-final");
-
-// // Initial Varibales with different sets of characters to select from
-
-// let specialChars = " !\"/#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-// let numChars = "0123456789";
-// let lowerChars = "abcdefghijklmnopqrstuvwxyz";
-// let upperChars = lowerChars.toUpperCase();
-
-// let charListFinal = "";
-
-// // Helper function to check and make sure user selected password length between 8-128
-
-// let o = function () {
-//     alert("You must select a password length between 8 and 128");
-//     passLength = prompt("How many characters would you like your password to be? Must be between 8 and 128.");
-//     passLengthChecker = Number(passLength);
-// }
-
-// // Helper function to check to make sure at least one type of character is selected
-
-// let choseCharList = function () {
-//     alert("You must select at least one of the 4 character types.");
-//     charTypeSpec = confirm("Would you like special characters?");
-//     charTypeNum = confirm("Would you like numeric characters?");
-//     charTypeLower = confirm("Would you like lowercase characters?");
-//     charTypeUpper = confirm("Would you like uppercase characters?");
-    
-// }
-// // Helper function to make final character list to select from for password generation and then returns the completed list
-
-// let generateFinalCharList = function () {
-    
-//     if (charTypeSpec === true) {
-//         charListFinal += specialChars;
-//     }
-//     if (charTypeNum === true) {
-//         charListFinal += numChars;
-//     }
-//     if (charTypeLower === true) {
-//         charListFinal += lowerChars;
-//     }
-//     if (charTypeUpper === true) {
-//         charListFinal += upperChars;
-//     }
-//     return charListFinal;
-// }
-
-// // Function to generate a random password and place password into password div
-
-// let generatePass = function () {
-//     event.preventDefault();
-//     let pass = "";
-//     let charList = generateFinalCharList()
-//     for (let i = 0; i < passLength; i ++) {
-//         pass += charList[Math.floor(Math.random() * charList.length)];
-//     }
-//     passwordBox.textContent = pass;
-// }
-
-// // Copy generated password to the clipboard
-
-// let copyText = function () {
-//     let copyText = document.getElementById("pass-final");
-//     let textArea = document.createElement("textarea");
-//     textArea.value = copyText.textContent;
-//     document.body.appendChild(textArea);
-//     textArea.select();
-//     document.execCommand("Copy");
-//     alert("Your generated password has been copied to the clipboard!");
-//     textArea.remove();
-//    }
-
-// // When buttons are clicked event listeners
-
-// generateBtn.addEventListener("click", generatePass);
-// copyBtn.addEventListener("click" , copyText);
-
-// // While loops until the user selects a character type
-// while (passLengthChecker < 8 || passLengthChecker > 128) {
-//     o();
-// }
-// while (charTypeLower == false && charTypeNum == false && charTypeSpec == false && charTypeUpper === false) {
-//     choseCharList();
-// }
